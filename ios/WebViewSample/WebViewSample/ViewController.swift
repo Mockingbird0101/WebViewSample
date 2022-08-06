5ecb0ec12a570dfcb64d40ad213be0239edae4b2
Mocking Bird <green.onyx0808@gmail.com>
2022-08-06 15:48:50 +0900

    iosプロジェクト作成

---
+++ ios/WebViewSample/WebViewSample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WebViewSample
+//
+//  Created by mockingbird on 2022/08/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
